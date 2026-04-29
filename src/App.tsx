bb3eae93c0501c90007e6bd36b0f5f2592252ea2
Katri Lotamõis <katrilotamois@outlook.com>
2026-04-29 15:52:09 +0300

    get movie titles from API

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import './App.css'
 import {useEffect, useState} from "react";
 import axios from 'axios'
 
+interface Movies {
+  id: number
+  title: string
+  poster_path: string
+  release_date: string
+}
+
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState<Movies[]>([])
 
   const apiKey =  "a2006311928939b35613c28405038c87"
   const popularMoviesUrl = "https://api.themoviedb.org/3/movie/popular"
 
   const fetchMovies = async () => {
     axios.get(`${popularMoviesUrl}?api_key=${apiKey}`).then((response: { data: any; }) => {
-      const result = response.data
+      const result = response.data.results
       console.log(result)
-    })
+      setMovies(result)
+    });
   }
 
   useEffect(() => {
@@ -20,7 +28,13 @@ function App() {
   }, [])
 
   return (
-    <div>Hello from React API calls</div>
+    <div>
+      {movies.map((m) => (
+          <div>
+            <h1>{m.title}</h1>
+          </div>
+      ))}
+    </div>
   )
 }
 
